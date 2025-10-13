ec080d7ff9ee51757cf8d6bde4df593d963e1b60
Sheu Ahmad <sheuahmad@gmail.com>
2025-10-13 02:31:28 +0100

    render..

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = 'https://tnlbeauty-api.onrender.com; 
+const API_BASE_URL = 'https://tnlbeauty-api.onrender.com'; 
 
 // DTO Interfaces (similar to reference)
 export interface AddIncomeDto {
